d96cd8634b75cae57e694e8d70f610fcc85e3323
Nick Mensah <103342287+mensahTribeWeb@users.noreply.github.com>
2024-06-18 03:49:35 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,14 @@ export default function About() {
     <section className="about">
       <h2 className="about--title">About</h2>
       <p className="about--p">
-        A innovative, analytical and organized problem solver, Java programming and Web Developer. 
+        An innovative, analytical, and organized problem solver, Java
+        programming and Web Developer.
       </p>
       <p className="about--p">
-        I am eager to build and develop projects around other motivated individuals in supportive environments. Effective and efficient communication are fundamental principles I live and stand by.
+        I am eager to build and develop projects with other motivated
+        individuals in supportive environments. Effective and efficient
+        communication are fundamental principles I live and stand by.
       </p>
     </section>
-  )
-};
+  );
+}
