dc33db3458cc4b851a41ead78f17df1cbd76a3d6
Nick Mensah <103342287+mensahTribeWeb@users.noreply.github.com>
2024-06-18 03:54:15 -0500

    Update Interests.js

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,13 +1,41 @@
 import React from "react";
 
-export default function Interest (){
-  return(
-    <section className="about">
-      <h2 className="about--title">Interests</h2>
-      <p className="about--interests">
-        Disney expert. World Traveler. Avid Reader. Understanding world languages/ cultures. Yerba Mate lover. Internet suffer. 
-      </p>
-      
+export default function Interest() {
+  return (
+    <section className="interest">
+      <h2 className="interest--title">Interests</h2>
+      <ul className="interest--list">
+        <li className="interest--item">
+          <span role="img" aria-label="Travel">
+            🌍
+          </span>{" "}
+          Learning about cultures through travel
+        </li>
+        <li className="interest--item">
+          <span role="img" aria-label="Coding">
+            💻
+          </span>{" "}
+          Coding/Programming
+        </li>
+        <li className="interest--item">
+          <span role="img" aria-label="Web Security">
+            🔐
+          </span>{" "}
+          Web Security
+        </li>
+        <li className="interest--item">
+          <span role="img" aria-label="Java">
+            🍵
+          </span>{" "}
+          Java/Spring
+        </li>
+        <li className="interest--item">
+          <span role="img" aria-label="Reading">
+            📖
+          </span>{" "}
+          Reading
+        </li>
+      </ul>
     </section>
   );
 }
