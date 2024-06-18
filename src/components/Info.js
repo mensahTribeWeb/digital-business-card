62ee28f6b810cde5392e61833056de6f247e6f5f
Nick Mensah <103342287+mensahTribeWeb@users.noreply.github.com>
2024-06-18 03:48:42 -0500

    Update Info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,35 +1,32 @@
 import React from "react";
-// import NickPro from "https://nickmensahportfolio.com/";
 import Nick from "../img/nickMensah.png";
 import email from "../img/mail.png";
 import linkedIn from "../img/linkedin.png";
 
-
 export default function Info() {
-  return(
+  return (
     <div className="info">
-      <img src={Nick}alt=" Nick Mensah" />
+      <img src={Nick} alt="Nick Mensah" />
       <h1 className="info--name">Nick Mensah</h1>
       <h2 className="info--title">Java Engineer</h2>
-      <h3 info--website>
-        <a href="https://nickmensahportfolio.com/">NickMensah.website</a>
+      <h3 className="info--website">
+        <a href="https://nokore.dev/">NickMensah.website</a>
       </h3>
 
       <div className="info--btn">
-      <a href="mailto:nick.d.mensah@gmail.com">
-        <button className="info--btn--email  info--btn--btn" type="email">
-          <img className="info--btn--email--icon icon" src={email} alt="" />
-          Email
-        </button>
+        <a href="mailto:nick.d.mensah@gmail.com">
+          <button className="info--btn--email info--btn--btn">
+            <img className="icon" src={email} alt="" />
+            Email
+          </button>
         </a>
         <a href="https://www.linkedin.com/in/nicholas-d-mensah/">
-        <button className="info--btn--linkedin info--btn--btn" >
-           <img className="info--btn--linkedin--icon icon" src={linkedIn} alt="" />
-           LinkedIn
-        </button>
-        </a>  
+          <button className="info--btn--linkedin info--btn--btn">
+            <img className="icon" src={linkedIn} alt="" />
+            LinkedIn
+          </button>
+        </a>
       </div>
-      
     </div>
   );
 }
