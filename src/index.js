a63330b518bf23ea9e6a1fb548e07ab59b32b828
Nick Mensah(Nokore.dev) <ndm.acct85@gmail.com>
2024-06-20 01:11:18 -0500

    Deploy

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-import './index.css';
-import App from './containers/App';
+import React from "react";
+import { createRoot } from "react-dom";
+import "./index.css";
+import App from "./containers/App";
 
-
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
